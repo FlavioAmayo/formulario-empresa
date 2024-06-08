880299724209cc180992bf421a6a2eb222c6855c
Flavio Amayo <famayo@unitru.edu.pe>
2024-06-08 00:46:16 -0500

    Primer commit - Proyecto de Laravel

---
+++ database/migrations/2024_06_08_041933_create_persona_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('persona', function (Blueprint $table) {
+            $table->bigIncrements('nPerCodigo');
+            $table->string('cPerApellido', 50)->nullable();
+            $table->string('cPerNombre', 50)->nullable();
+            $table->string('cPerDireccion', 100)->nullable();
+            $table->date('dPerFecNac')->nullable();
+            $table->integer('nPerEdad')->nullable();
+            $table->decimal('nPerSueldo', 6, 2)->nullable();
+            $table->string('cPerRnd', 50)->nullable();
+            $table->char('nPerEstado', 1)->default('1');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('persona');
+    }
+}
